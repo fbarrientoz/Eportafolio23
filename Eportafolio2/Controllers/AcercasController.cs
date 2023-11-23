5214bd19c4742a2330d26902e89bf3f0c1d845d7
Fabiola <Fabiola@LAPTOP-5NFTL38M>
2023-11-23 01:46:23 -0800

    Se incluyo CRUD para todos los modelos

---
+++ Eportafolio2/Controllers/AcercasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Eportafolio2.Models;
+
+namespace Eportafolio2.Controllers
+{
+    public class AcercasController : Controller
+    {
+        private amartinezEntities db = new amartinezEntities();
+
+        // GET: Acercas
+
+        public ActionResult Index()
+        {
+            if(User.Identity.IsAuthenticated)
+            { 
+                return View(db.Acercas.ToList());
+            }
+
+            else
+            {
+                return View(db.Acercas.ToList());
+            }
+        }
+
+        // GET: Acercas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Acerca acerca = db.Acercas.Find(id);
+            if (acerca == null)
+            {
+                return HttpNotFound();
+            }
+            return View(acerca);
+        }
+
+        // GET: Acercas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Acercas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Acerca1")] Acerca acerca)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Acercas.Add(acerca);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(acerca);
+        }
+
+        // GET: Acercas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Acerca acerca = db.Acercas.Find(id);
+            if (acerca == null)
+            {
+                return HttpNotFound();
+            }
+            return View(acerca);
+        }
+
+        // POST: Acercas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Acerca1")] Acerca acerca)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(acerca).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(acerca);
+        }
+
+        // GET: Acercas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Acerca acerca = db.Acercas.Find(id);
+            if (acerca == null)
+            {
+                return HttpNotFound();
+            }
+            return View(acerca);
+        }
+
+        // POST: Acercas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Acerca acerca = db.Acercas.Find(id);
+            db.Acercas.Remove(acerca);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Eportafolio2/Controllers/ProyectoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Eportafolio2.Models;
+
+namespace Eportafolio2.Controllers
+{
+    public class ProyectoController : Controller
+    {
+        private amartinezEntities db = new amartinezEntities();
+
+        // GET: Proyecto
+        public ActionResult Index()
+        {
+            return View(db.Proyectos.ToList());
+        }
+
+        // GET: Proyecto/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proyecto proyecto = db.Proyectos.Find(id);
+            if (proyecto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proyecto);
+        }
+
+        // GET: Proyecto/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Proyecto/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombre,Descripcion,Imagen,Empresa,Fecha")] Proyecto proyecto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Proyectos.Add(proyecto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(proyecto);
+        }
+
+        // GET: Proyecto/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proyecto proyecto = db.Proyectos.Find(id);
+            if (proyecto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proyecto);
+        }
+
+        // POST: Proyecto/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombre,Descripcion,Imagen,Empresa,Fecha")] Proyecto proyecto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(proyecto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(proyecto);
+        }
+
+        // GET: Proyecto/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Proyecto proyecto = db.Proyectos.Find(id);
+            if (proyecto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proyecto);
+        }
+
+        // POST: Proyecto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Proyecto proyecto = db.Proyectos.Find(id);
+            db.Proyectos.Remove(proyecto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
